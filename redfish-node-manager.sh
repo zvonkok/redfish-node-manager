c8e5bcd1f7afc34a882b93add6d5d18caf3a4654
Zvonko Kaiser <zkaiser@nvidia.com>
2023-09-22 12:29:32 +0000

    # SQUASH

--- redfish-node-manager.sh
+++ redfish-node-manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 set -e 
-#set -x
+set -x
 
 REDFISH_IP=""
 REDFISH_USER="$1"
@@ -14,9 +14,14 @@ get_redfish_ip () {
         table42=$(dmidecode -t 42)
         redfish_ip_format=$(echo "$table42" | grep "Redfish Service IP Address Format" | cut -d':' -f2 | tr -d '[:space:]')
         REDFISH_IP=$(echo "$table42" | grep "${redfish_ip_format} Redfish Service Address" | cut -d':' -f2 | tr -d '[:space:]')
+        # GENOA 10.153.145.96
+        REDFISH_IP=10.153.145.96
 }
 
 
 get_redfish_ip
 
-curl -k https://"${REDFISH_IP}"/redfish/v1/Systems/1/Bios -u  "${REDFISH_USER}:${REDFISH_PASSWD}"  | jq .
+
+curl -k https://"${REDFISH_IP}":443/redfish/v1 | jq . 
+
+#curl -k https://"${REDFISH_IP}"/redfish/v1/Systems/1/Bios -u  "${REDFISH_USER}:${REDFISH_PASSWD}"  | jq .
